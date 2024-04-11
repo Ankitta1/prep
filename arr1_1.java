af5f98a914932c7a40e16f059c5964afff8ff019
Ankitta1 <ankitayankanchi05@gmail.com>
2024-04-11 18:36:42 +0530

    push

---
+++ arr1_1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class arr1_1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
